b8d77b7a973ce213ad3e0e618260573b38bb7afb
joaoh <>
2024-11-04 02:07:02 -0300

    Chat iface implemented

--- src/Repositories/OllamaRepository.cs
+++ src/Repositories/OllamaRepository.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using OllamaLib.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace OllamaLib.Repositories
@@ -10,20 +13,48 @@ namespace OllamaLib.Repositories
     {
         private static HttpClient _httpClient = new HttpClient();
         private static string _apiUrl = "http://localhost:11434";
+        private static string _model = "";
 
         /// <summary>
-        /// Set Ollama API url (http://localhost:11434 is default)
+        /// Set Ollama API url
+        /// <param name="apiUrl">Ollama api url, default is http://localhost:11434.</param>
         /// </summary>
         public static void SetApi(string apiUrl)
         {
             _apiUrl = apiUrl;
         }
 
-        public static async Task<string> Chat(string message)
+        public static void SetModel(string model)
         {
-            var chatPostData = new
-            
-            using HttpResponseMessage response = await _httpClient.PostAsync($"{apiUrl}/api/chat");
+            _model = model;
+        }
+
+        /// <summary>
+        /// Sends a chat message to the AI model and retrieves the response asynchronously.
+        /// </summary>
+        /// <param name="role">The role of the message sender, such as "user" or "assistant".</param>
+        /// <param name="message">The content of the message to be sent to the AI model.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the response from the AI model as a string.</returns>
+        public static async Task<string> Chat(string role, string message)
+        {
+            var chatPostData = new ChatPostData.Root()
+            {
+                model = _model,
+                messages = new List<ChatPostData.Message>
+                {
+                    new ChatPostData.Message
+                    {
+                        role = role,
+                        content = message
+                    }
+                }
+            };
+
+            string chatPostDataJson = JsonSerializer.Serialize(chatPostData);
+            var finalPostData = new StringContent(chatPostDataJson, Encoding.UTF8, "application/json");
+            using HttpResponseMessage response = await _httpClient.PostAsync($"{_apiUrl}/api/chat", finalPostData);
+
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
